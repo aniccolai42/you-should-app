7016f0be1b87c010564cf657a7807df9c751fab5
Andrew Niccolai <andrew.niccolai@erdc.dren.mil>
2023-01-16 17:51:04 -0500

    Change app title to Should Do

--- web.py
+++ web.py
@@ -4,15 +4,16 @@ import functions
 todos = functions.get_todos()
 
 
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
 
-st.title("My Todo App")
-st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.title("Should Do App")
+st.subheader(f"This is the Niccolai family 'should-do' app")
+st.write("This app is to increase our productivity")
 
 
 for index, todo in enumerate(todos):
